b99bb6279497bb1a2cb81b0b05d5e3d5ea97bba2
Cynthia Siew <cynsiewsq@gmail.com>
2021-05-11 05:50:02 +0000

    version 0.2.0

--- R/spreadr.R
+++ R/spreadr.R
@@ -1,137 +1,201 @@
-#' Spread of activation process to determine activation of network nodes.
+#' Simulate spreading activation in a network
 #'
-#' This function takes in a dataframe with 'node' and 'activation' columns, and
-#' simulates the spread of activation among nodes in a specified network structure.
+#' @param network Adjacency matrix or
+#'   \code{\link[igraph:igraph-package]{igraph}} object representing the network
+#'   in which to simulate spreading activation.
+#' @param start_run Non-empty \code{\link[base]{data.frame}} with mandatory
+#'   columns \emph{node}, \emph{activation}; and optional columns \emph{time}.
+#'   If the \emph{time} column is present, \emph{activation} is added to
+#'   \emph{node} at each \emph{time}. Otherwise, the \emph{activation}s are
+#'   added to their corresponding nodes at \code{t = 0}.
+#' @param decay Number from 0 to 1 (inclusive) representing the proportion of
+#'   activation that is lost at each time step.
+#' @param retention Number from 0 to 1 (inclusive) or a numeric vector of such
+#'   numbers of length equals number of nodes in the network. This represents
+#'   the proportion of activation that remains in the node (not spread) at each
+#'   time step. Then, \code{1 - retention} of the activation at each node is
+#'   spread to neighbouring nodes. If a numeric vector, retentions are assigned
+#'   to nodes according to the order given by \code{V(network)} if
+#'   \code{network} is an \code{\link[igraph:igraph-package]{igraph}} object or
+#'   \code{nrow(network)} if \code{network} is an adjacency matrix.
+#' @param suppress Number representing the maximum amount of activation in a
+#'   node for it to be set to 0, at each time step.
+#' @param time Positive non-zero integer, or \code{NULL}. If not \code{NULL},
+#'   the number of time steps to simulate before stopping. Otherwise,
+#'   stop with the \code{threshold_to_stop} parameter.
+#' @param threshold_to_stop Number or \code{NULL}. If not \code{NULL}, stop the
+#'   simulation only when all nodes have activation value less than
+#'   \code{threshold_to_stop}. Otherwise, stop with the \code{time} parameter.
+#' @param include_t0 Boolean flag indicating if activation at \code{t = 0}
+#'   should be prepended to the output \code{data.frame}. This is \code{FALSE}
+#'   by default for back-compatibility.
+#' @param create_names Boolean flag indicating if nodes should be automatically
+#'   named (\code{1:n}, where \code{n} is the number of nodes) in case they are
+#'   missing.
+#' @param never_stop Boolean flag indicating if the simulation should be stopped
+#'   if there have been too many iterations (so that there might be an infinite
+#'   loop).
 #'
-#' @param start_run A non-empty dataframe with 'node' and 'activation' columns. Must be specified.
-#' @param decay Proportion of activation that is lost at each time step. Ranges from 0 to 1. Default is 0.
-#' @param retention Proportion of activation that remains in the node from which activation is being spread from. Ranges from 0 to 1. Default is 0.5.
-#' @param suppress Suppress nodes with a final activation of < x units at each time step to an activation value of 0. Default is 0.
-#' @param network Network where the spreading occurs. Must be specified. Must be an igraph object with a "name" property or an adjacency matrix.
-#' @param time Number of time steps to run the spreadr() function for. Default is 10.
-#' @param create_names Name nodes 1:number_of_nodes in case network is missing node names.
-#' @return A compiled dataframe with 'node', 'activation' and 'time' columns showing the spread of activation in the network over time.
+#' @return A \code{\link[base]{data.frame}} with \emph{node}, \emph{activation}
+#'   and \emph{time} columns representing the spread of activation in the network
+#'   over time.
+#'
+#' @details At least one of parameters \code{time} or \code{threshold_to_stop}
+#'   must be non-\code{NULL}. If both are non-\code{NULL}, the simulation stops
+#'   at the earliest time possible.
+#'
+#'   The simulation iterates like so: for every \code{i} in \code{[0, time]},
+#'   \itemize{
+#'     \item{Spread activation from node to node}
+#'     \item{Decay the activation at each node by the proportion specified by
+#'       \code{decay}}
+#'     \item{Set the activation at nodes with activation less than
+#'       \code{suppress} to \code{0}}
+#'     \item{Add the activations in \code{start_run} with \code{time = i} to
+#'       their corresponding nodes}
+#'     \item{Save the activations at each node for output}
+#'     \item{Check the terminating conditions \code{time} and
+#'       \code{threshold_to_stop}. If any are satisfied, terminate the
+#'       simulation.}
+#'   }
+#'
 #' @examples
-#' g_d_mat <- matrix(sample(c(0,1), 100, replace = TRUE), 10, 10) 
-#' # make an adjacency matrix and randomly fill some cells with 1s 
-#' diag(g_d_mat) <- 0 # remove self-loops 
-#' library(spreadr)
-#' initial_df <- data.frame(node = 1, activation = 20, stringsAsFactors = FALSE)
-#' result_d <- spreadr(start_run = initial_df, decay = 0, retention = 0.5, 
-#' suppress = 0, network = g_d_mat, time = 10) 
-#' head(result_d, 10)
-#' tail(result_d, 10)
+#' # make an adjacency matrix and randomly fill some cells with 1s
+#' mat <- matrix(sample(c(0,1), 100, replace=TRUE), 10, 10)
+#' diag(mat) <- 0 # remove self-loops
+#' initial_df <- data.frame(node=1, activation=20, stringsAsFactors=FALSE)
+#' results <- spreadr(mat, initial_df)
+#'
+#' head(results, 10)
+#' tail(results, 10)
+#'
+#' @importFrom assertthat assert_that is.count is.flag has_name
+#' @importFrom igraph as_adjacency_matrix graph_from_adjacency_matrix is.igraph
+#'   V V<-
+#' @importFrom Rcpp sourceCpp
+#' @useDynLib spreadr
+#'
 #' @export
-
-
 spreadr <- function(
-  network, # an `igraph` network object or an adjacency matrix
-  start_run, # data.frame 'initial_df' with node and activation columns to specify activation at t = 0
-  retention = 0.5, # retention parameter
-  time = 10, # number of time steps
-  decay = 0, # decay paramater
-  suppress = 0, # suppress parameter,
-  create_names = TRUE # name nodes 1:size if needed
-){
-  ### ERROR MESSAGES ###
-  # check if start_run is in the correct format
-  if (is.data.frame(start_run) == F || colnames(start_run) != c('node', 'activation')) {
-    stop('Initial activation dataframe is not in the correct format. Must be a dataframe with -node-
-         and -activation- columns.')
+    network, start_run, retention=0.5, time=10, threshold_to_stop=NULL,
+    decay=0, suppress=0, include_t0=FALSE, create_names=TRUE, never_stop=FALSE
+  ){
+
+  # is network an igraph or square matrix-like object?
+  assert_that(is.igraph(network) ||
+    length(dim(network)) == 2 && dim(network)[1] == dim(network)[2])
+  # is start_run in the correct format?
+  assert_that(is.data.frame(start_run))
+  assert_that(has_name(start_run, "node"), has_name(start_run, "activation"))
+  assert_that(  # time is non-negative integer, if it is provided
+    !has_name(start_run, "time") ||
+      all(sapply(start_run$time, function(x) x == 0 || is.count(x))))
+  # is decay is a number between 0 and 1 inclusive?
+  assert_that(decay >= 0 && decay <= 1)
+  # is retention an appropriate number or numeric vector?
+  assert_that(is.numeric(retention))
+  n_nodes <- if (is.igraph(network)) length(V(network)) else nrow(network)
+  assert_that(length(retention) == 1 || length(retention) == n_nodes)
+  assert_that(all(retention >= 0 & retention <= 1))
+  # are terminating conditions ok? (time and threshold_to_stop)
+  assert_that(
+    !(is.null(time) && is.null(threshold_to_stop)),
+    msg="time and threshold_to_stop cannot both be NULL")
+  assert_that(is.null(time) || is.count(time))
+  assert_that(is.null(threshold_to_stop) || threshold_to_stop > 0)
+  # are node names ok?
+  does_network_have_names <- if (is.igraph(network))
+    !is.null(V(network)$name) else
+    !is.null(colnames(network)) || !is.null(rownames(network))
+  assert_that(create_names || does_network_have_names)
+  # is return_type ok?
+  assert_that(is.flag(include_t0))
+
+  # honour create_names: if TRUE, add node names
+  if (create_names && !does_network_have_names && is.igraph(network)) {
+    V(network)$name <- 1:length(V(network))
+  } else if (create_names && !does_network_have_names && !is.igraph(network)) {
+    colnames(network) <- 1:ncol(network)
+    rownames(network) <- 1:nrow(network)
   }
 
-  # check if the node column in start_run is a factor (if so, it must be converted to character class)
-  if (is.factor(start_run$node) == T) {
-    start_run$node <- as.character(start_run$node)
-  }
+  # we work with adjacency matrices, so if network is.igraph, convert to an
+  # adjacency matrix
+  if (is.igraph(network)) network <- as_adjacency_matrix(network)
+  assert_that(all(colnames(network) == rownames(network)))
 
-  # check if decay is a number from 0 to 1
-  if (decay < 0 || decay > 1) {
-    stop('Decay value is not a number from 0 to 1.')
-  }
+  # is there any node in start_run which does not exist in network?
+  not_exist_idx <- which(!start_run$node %in% colnames(network))
+  not_exist_node <- start_run$node[not_exist_idx]
+  not_exist_node <- unique(not_exist_node)
+  if (length(not_exist_node) > 0) warning(
+    "These nodes specified in start_run don't exist in network: ",
+    paste0(not_exist_node, collapse=", "))
 
-  # check if retention is a number from 0 to 1
-  if (retention < 0 || retention > 1) {
-    stop('Retention value is not a number from 0 to 1.')
-  }
+  # it is easier if we assume retention is always a vector
+  if (length(retention) == 1) retention <- rep(retention, n_nodes)
 
-  # check if time is a non-negative number
-  if (time < 0 || is.numeric(time) == F) {
-    stop('Something is off with the time value.')
-  }
+  # variables in the loop:
+  # -   d :: numeric integer vector, the degree of each node
+  # - a_t :: numeric vector, the activation at time t
+  # - mat :: base::matrix version of network
+  d <- Matrix::rowSums(network)
+  n_nodes <- length(d)
+  a_t <- rep(0, n_nodes) # empty vector
+  mat <- as.matrix(network)
 
-  ### SET UP ###
-  ## CONVERT IGRAPH TO ADJMAT IF NEEDED, GET DEGREE ##
-  if(igraph::is.igraph(network) == T) { # if an igraph object
+  # pre-loop set-up: if !has_name(start_run, "time"), start_run specifies the
+  # activation value of certain nodes at t=0. Otherwise, start_run specifies
+  # when to add how much activation to certain nodes, so check for activations
+  # to be added at t=0.
+  if (!has_name(start_run, "time")) start_run$time <- 0
 
-    # check if the igraph object has a name attribute
-    if (is.null(igraph::V(network)$name) == T) {
-      if(create_names == TRUE){
-        igraph::V(network)$name = 1:length(igraph::V(network))
-      } else {
-        stop('Network does not have a "name" attribute.')
-      }
-    }
-
-    # check if node labels are unique
-    if (length(unique(igraph::V(network)$name)) != igraph::gorder(network)) {
-      stop('Nodes need to have unique labels.')
-    }
-    if(igraph::is.weighted(network) == T) { # if graph is weighted
-      mat<-igraph::as_adj(network, sparse = F, attr="weight") # to keep the weights in a weighted graph
-      d <- colSums(mat) # GET THE DEGREE OF EACH NODE
-      if(is.null(names(d))) names(d) <- 1:length(d) # names are simply column numbers
-    } else {
-      mat<-igraph::as_adj(network, sparse = F) # unweighted graph
-      d <- colSums(mat) # GET THE DEGREE OF EACH NODE
-      if(is.null(names(d))) names(d) <- 1:length(d) # names are simply column numbers
-    }
-  } else {
-    mat <- network # input is already an adj matrix
-    d <- colSums(mat) # GET THE DEGREE OF EACH NODE
-    names(d) <- 1:length(d) # names are simply column numbers
-  }
-
-  ### SET UP FOR SPREADING ACTIVATION T = 0 ###
-  ## CREATE THE ACTIVATION VECTOR ##
-  n_nodes = length(d)
-  a <- rep(0, n_nodes) # empty vector
-  names(a) <- names(d)
-  a[start_run$node] <- start_run$activation # add the activation input in; multiple values ok
-
-  ## OBJECT TO STORE RESULTS ##
-  out_df <- data.frame(node = names(a), activation = a, time = 0L)
-
-  ## ACTIVATION AT TIME T ##
-  a_t <- a
-
-  ## REPEAT PROCESS FOR EACH TIME STEP
-  activations = numeric(time * n_nodes)
-  for (i in seq_len(time)){
-    # ACTIVATION AT TIME T - 1
+  current_time <- 0
+  activations <- numeric(0)
+  while (TRUE) {
+    # spreading activation
     a_tm1 <- a_t
-
-    # # ACTIVATION MATRIX AT TIME T
-    # mat_t <- apply(mat, 1, function(x) (1 - retention) * x * a_tm1/d) # this generalizes to weighted networks
-    # mat_t[is.nan(mat_t)] <- 0 # in case there are hermits in the network, convert NaNs to 0
-    # diag(mat_t) <- retention * a_tm1
-    mat_t = create_mat_t(mat, a_tm1, d, retention)
-
-    # UPDATED ACTIVATION VECTOR
+    mat_t <- create_mat_t(mat, a_tm1, d, retention)
+    # create_mat_t creates an "activation matrix", where each (i, j) entry is
+    # the activation at time t, at node j, due to node i.
     a_t <- colSums(mat_t)
-    a_t <- a_t * (1 - decay)  # decay proportion of activation at the end of each time step
-    a_t[a_t < suppress] <- 0 # reduce activation to 0 cells less than suppress parameter
 
-    # STORE RESULTS
-    activations[((i-1) * n_nodes + 1) : (i * n_nodes)] = a_t
+    # decay and suppress
+    a_t <- (1-decay) * a_t
+    a_t[a_t < suppress] <- 0
+
+    # apply start_run instructions
+    start_run_t <- subset(start_run, time == current_time)
+    for (i in 1:nrow(start_run_t)) {
+      j <- which(colnames(network) == start_run_t[i, "node"])
+      a_t[j] <- a_t[j] + start_run_t[i, "activation"]
+    }
+
+    # record
+    activations <- c(activations, a_t)
+
+    # check termination
+    if (!is.null(time) && current_time >= time) break
+    if (!is.null(threshold_to_stop) && all(a_t < threshold_to_stop)) break
+    if (is.null(time) && decay == 0 && !never_stop && current_time > 10000) {
+      warning(
+        "Stopping because there might potentially be an infinite loop. ",
+        "Set NEVER_STOP=TRUE to override.")
+      break
+    }
+
+    current_time <- current_time + 1
   }
 
-  # OTHER OUT STUFF
-  nodes = rep(names(d), time) # keep original names of graph
-  is = rep(1:time, rep(n_nodes, time))
+  nodes <- rep(colnames(network), current_time+1)
+  is <- rep(0:(current_time), rep(n_nodes, current_time+1))
+  return_df <- data.frame(
+    node=nodes, activation=activations, time=is, stringsAsFactors=TRUE)
 
-  return(data.frame('node' = nodes, # spread_fast does not include t=0
-                    'activation' = activations,
-                    'time' = is))
+  if (!include_t0) {
+    return_df <- subset(return_df, time != 0)
+    if (nrow(return_df) != 0) rownames(return_df) <- 1:nrow(return_df)
   }
 
+  return_df
+}
--- inst/doc/spreadr_vignette.R
+++ inst/doc/spreadr_vignette.R
@@ -1,81 +1,127 @@
-## ---- eval=TRUE----------------------------------------------------------
-options(stringsAsFactors = FALSE) # to prevent strings from being converted to a factor class
-extrafont::loadfonts(quiet=TRUE)
+## ----eval=FALSE---------------------------------------------------------------
+#  install.packages("spreadr")
 
-# install.packages('devtools')
-# library(devtools)
-# install_github('csqsiew/spreadr') # download spreadr from my github page
+## ----eval=FALSE---------------------------------------------------------------
+#  install.packages("remotes")
+#  remotes::install_github("csqsiew/spreadr")
+
+## ----message=FALSE------------------------------------------------------------
 library(spreadr)
+library(igraph)
 
-## ---- warning=FALSE------------------------------------------------------
+data("pnet")  # load and inspect the igraph object
+
+## ----eval=FALSE---------------------------------------------------------------
+#  plot(pnet)
+
+## ----echo=FALSE---------------------------------------------------------------
+set.seed(1)
+layout <- layout_with_lgl(pnet)
+plot(pnet, layout=layout)
+
+## ----message=FALSE------------------------------------------------------------
+library(spreadr)
 library(igraph)
 set.seed(1)
 
-g <- sample_gnp(20, 0.2, directed = F, loops = F) # make a random network
-V(g)$name <- paste0('N', as.character(1:gorder(g))) # give some meaningful labels for the 'name' attribute
+data("pnetm")
+pnetm[1:5, 1:5]  # inspect the first few entries
 
-V(g)$color <- c('blue', rep('white', 19))
-l <- layout_with_fr(g) # to save the layout 
-plot(g, l = l, vertex.size = 10, vertex.label.dist = 2, vertex.label.cex = 0.8)
-# the blue node will be assigned activation at t = 0
+## ----eval=FALSE---------------------------------------------------------------
+#  plot(graph_from_adjacency_matrix(
+#    pnetm, mode="undirected")) # visualise the graph
 
-## ------------------------------------------------------------------------
-initial_df <- data.frame(node = 'N1', activation = 20, stringsAsFactors = F)
-initial_df
+## ----echo=FALSE---------------------------------------------------------------
+plot(graph_from_adjacency_matrix(
+    pnetm, mode="undirected"),
+  layout=layout)
 
-## ------------------------------------------------------------------------
-result <- spreadr::spreadr(start_run = initial_df, decay = 0,
-                              retention = 0.5, suppress = 0,
-                              network = g, time = 10) 
+## -----------------------------------------------------------------------------
+start_run <- data.frame(
+  node=      c("beach", "speck"),
+  activation=c(     20,      10))
 
-## ------------------------------------------------------------------------
-head(result, 10) # view the results
-tail(result, 10)
-# write.csv(result, file = 'result.csv') # save the results 
+## -----------------------------------------------------------------------------
+start_run <- data.frame(
+  node =       c("beach", "speck"),
+  activation = c(     20,      10),
+  time =       c(      0,       5))
 
-library(ggplot2) 
-a1 <- data.frame(node = 'N1', activation = 20, time = 0) # add back initial activation at t = 0
-result_t0 <- rbind(result,a1)
-ggplot(data = result_t0, aes(x = time, y = activation, color = node, group = node)) +
-  geom_point() + geom_line() + ggtitle('unweighted, undirected network') # visualize the results 
+## ----echo=FALSE---------------------------------------------------------------
+start_run <- data.frame(
+  node =       c("beach", "speck"),
+  activation = c(     20,      10))
 
-## ------------------------------------------------------------------------
-# weighted, undirected network example 
-g_w <- g
-set.seed(2)
-E(g_w)$weight <- runif(gsize(g_w)) # make the edges in the network have different weights ranging from 0 to 1 (excluding extreme values)
+## -----------------------------------------------------------------------------
+result <- spreadr(pnet, start_run, include_t0=TRUE)
 
-result_w <- spreadr::spreadr(start_run = initial_df, decay = 0,
-                              retention = 0.5, suppress = 0,
-                              network = g_w, time = 10) 
+## ----eval=TRUE----------------------------------------------------------------
+head(result)  # inspect the first few rows
+tail(result)  # inspect the last few rows
 
-result_w_t0 <- rbind(result_w, a1) # add back initial activation at t = 0
-ggplot(data = result_w_t0, aes(x = time, y = activation, color = node, group = node)) +
-  geom_point() + geom_line() + ggtitle('weighted, undirected network') # visualize the results 
+## ----eval=FALSE---------------------------------------------------------------
+#  write.csv(result, file="result.csv")  # save result as CSV file
 
-## ------------------------------------------------------------------------
-# unweighted, directed network example
-set.seed(3)
-g_d_mat <- matrix(sample(c(0,1), 100, replace = T), 10, 10) # make a matrix and randomly fill some cells with 1s 
-diag(g_d_mat) <- 0 # remove self-loops 
-g_d_mat
+## -----------------------------------------------------------------------------
+library(ggplot2)
+ggplot(result, aes(x=time, y=activation, color=node)) +
+  geom_point() +
+  geom_line()
 
-# spreadr will work on an adjacency matrix too 
-result_d <- spreadr::spreadr(start_run = data.frame(node = 1, activation = 20, stringsAsFactors = F), 
-                             decay = 0,
-                              retention = 0.5, suppress = 0,
-                              network = g_d_mat, time = 10) 
+## -----------------------------------------------------------------------------
+weighted_network <- matrix(
+  c(0, 1, 9,
+    1, 0, 0,
+    9, 0, 0), nrow=3, byrow=TRUE)
+colnames(weighted_network) <- c("a", "b", "c")
+rownames(weighted_network) <- c("a", "b", "c")
 
-result_d_t0 <- rbind(result_d, data.frame(node = 1, activation = 20, time = 0)) # add back initial activation at t = 0
-ggplot(data = result_d_t0, aes(x = time, y = activation, color = node, group = node)) +
-  geom_point() + geom_line() + ggtitle('unweighted, directed network') 
+# To visualise the network only --- this is not necessary for spreadr
+weighted_igraph <- graph_from_adjacency_matrix(
+  weighted_network, mode="undirected", weighted=TRUE)
+plot(weighted_igraph, edge.width=E(weighted_igraph)$weight)
 
-## ------------------------------------------------------------------------
-# first column 
-data.frame(node = 1:10, edge = g_d_mat[,1])
-# hence activation is going from node 1 to nodes 3,4,10 - as in the plot 
+## -----------------------------------------------------------------------------
+spreadr(
+  weighted_network, data.frame(node="a", activation=10),
+  time=1, retention=0, include_t0=TRUE)
 
-# first row - wrong direction!
-data.frame(node = 1:10, edge = g_d_mat[1,])
-# activation does NOT go from node 1 to nodes 3,4,5,7,8,10
+## -----------------------------------------------------------------------------
+directed_network <- matrix(
+  c(0, 1, 0,
+    0, 0, 1,
+    0, 0, 0), nrow=3, byrow=TRUE)
+colnames(directed_network) <- c("a", "b", "c")
+rownames(directed_network) <- c("a", "b", "c")
+
+# To visualise the network only --- this is not necessary for spreadr
+directed_igraph <- graph_from_adjacency_matrix(
+  directed_network, mode="directed")
+plot(directed_igraph, edge.width=E(directed_igraph)$weight)
+
+## -----------------------------------------------------------------------------
+spreadr(
+  directed_network, data.frame(node="b", activation=10),
+  time=1, retention=0, include_t0=TRUE)
+
+## -----------------------------------------------------------------------------
+params <- data.frame(
+  retention=c(0, 0.5,   0, 0.5),
+  decay=    c(0,   0, 0.5, 0.5))
+
+## -----------------------------------------------------------------------------
+network <- matrix(
+  c(0, 1,
+    0, 0), nrow=2, byrow=TRUE)
+start_run <- data.frame(node=1, activation=10)
+
+## -----------------------------------------------------------------------------
+apply(params, 1, function(row)
+  spreadr(
+    network, start_run,
+    time=2, include_t0=TRUE,
+    retention=row[1], decay=row[2]))
+
+## -----------------------------------------------------------------------------
+sessionInfo()
 
